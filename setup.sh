47af024961fb2236badff0a73e8cbe9e3d16b23f
David Avedissian <git@dga.me.uk>
2015-01-01 20:40:06 +0000

    Updated the setup script to specify a debug or release build

--- setup.sh
+++ setup.sh
@@ -2,5 +2,11 @@
 rm -rf build
 mkdir build
 pushd build
-cmake ..
+if ["$1" == "debug"]; then
+  cmake .. -DCMAKE_BUILD_TYPE=Debug
+elif ["$1" == "release"]; then
+  cmake .. -DCMAKE_BUILD_TYPE=Release
+else
+  cmake ..
+fi
 popd
